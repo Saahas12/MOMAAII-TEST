dce8c9e409c935c460020f50d4eb1501448cb9f3
Saahas12 <saahaspatkar@gmail.com>
2024-03-23 16:53:31 +0530

    made new sections of mens and made contact page

--- Frontend/js/script.js
+++ Frontend/js/script.js
@@ -8,3 +8,28 @@ dropIcon.addEventListener('click',function(){
       x.style.display = "block";
     }
 })
+
+
+// Get reference to the dropdown icon
+const dropdownIcon = document.getElementById('men-drop');
+
+// Get reference to the submenu
+const submenu = document.getElementById('sub-ul-1');
+
+// Add click event listener to the dropdown icon
+dropdownIcon.addEventListener('click', function() {
+    // Toggle visibility of the submenu
+    submenu.classList.toggle('visible');
+});
+
+
+const dropdownIcon1 = document.getElementById('women-drop');
+
+// Get reference to the submenu
+const submenu1 = document.getElementById('sub-ul-2');
+
+// Add click event listener to the dropdown icon
+dropdownIcon1.addEventListener('click', function() {
+    // Toggle visibility of the submenu
+    submenu1.classList.toggle('visible');
+});
